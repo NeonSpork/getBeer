c1fd4c423c41deb01c9deaeb26d852132b5644ef
NeonSpork <leiknes@gmail.com>
2018-08-22 13:35:06 +0200

    reworking little LCD screen

--- lcd_tester.py
+++ lcd_tester.py
@@ -1,11 +1,13 @@
 """LCD tester"""
 
+import RPi.GPIO as GPIO
 from RPLCD import CharLCD
 
 lcd = CharLCD(cols=16,
               rows=2,
               pin_rs=26,
               pin_e=24,
-              pins_data=[22, 18, 16, 12])
+              pins_data=[22, 18, 16, 12],
+              numbering_mode=GPIO.BOARD)
 
 lcd.write_string(u'Testing for BEER!')
