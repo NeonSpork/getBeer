0ed6eb1685b4855b8711bf433a86aaeccdd5b1da
NeonSpork <leiknes@gmail.com>
2018-09-05 19:31:26 +0200

    rework of menu

---
+++ getBeer.py
@@ -0,0 +1,231 @@
+"""getBeer_touch
+
+This version of the dispensor uses a 7 inch touch screen
+as the primary interface. This is designed to be build into
+a kegerator, and as such it has more permanent features like
+an automatic measurement of remaining volume via a load sensor
+(weight scale), temperature monitor, etc.
+
+Wiring the hardware:
+Magnetic valve  - GP5 (pin 29) & GND (pin 30)
+Temperature - GP4 (pin 7), GND (pin 20) & 3v3 (pin 1)
+Load sensor - VCC=3v3 (pin 17), GND (pin 9), DT=GP2 (pin 3), SCK=GP3 (pin 5)
+See LCD for wiring 16x2 display
+"""
+
+import pygame as pg
+from settings import *
+import RPi.GPIO as GPIO
+from hx711 import HX711
+from w1thermsensor import W1ThermSensor
+import lcd
+
+
+class BeerDispenser(object):
+    def __init__(self):
+        super(BeerDispenser, self).__init__()
+        pg.init()
+        self.screen = pg.display.set_mode((SWIDTH, SHEIGHT), pg.FULLSCREEN)
+        pg.mouse.set_visible(True)  # change to False when using touch screen
+        self.clock = pg.time.Clock()
+
+        # Sets numeration of channels according to GPIO on Pi
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+
+        # Set up GPIO pins
+        GPIO.setup(5, GPIO.OUT, initial=0)  # Magnetic valve, starts closed
+        GPIO.setup(4, GPIO.IN)  # Temperature probe DS18S20
+        GPIO.setup(2, GPIO.IN)  # Load sensor DT
+        GPIO.setup(3, GPIO.OUT)  # Load sensor SCK
+
+        # Load sensor
+        hx = HX711(2, 3)
+        hx.set_scale(92)  # Sets bit value for 1g
+        hx.set_offset(0)  # This gets calibrated to zero the sensor
+
+        # Parameters for dispenser
+        self.running = True
+        self.dispensing = False
+        self.buttonDown = False
+        self.bg_image = 0
+        self.dispenserDisplay = True
+        self.beerChooser = False
+        self.tempSensor = W1ThermSensor()
+
+    def drawToScreen(self, image, x, y):
+        self.image = image
+        image_rect = image.get_rect()
+        image_rect.center = (x, y)
+        self.screen.blit(image, image_rect)
+
+    def dispensorEvents(self):
+        self.mouse = pg.mouse.get_pos()
+        self.click = pg.mouse.get_pressed()
+        self.keys = pg.key.get_pressed()
+        if self.click[0] == 1:
+            if self.mouse[0] > (SWIDTH-(200*RELX)) and self.mouse[1] > (SHEIGHT-(200*RELY)):
+                self.openValve()
+        else:
+            self.shutValve()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+            if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                self.running = False
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                if self.mouse[0] < (50*RELX) and self.mouse[1] < (50*RELY):
+                    self.dispenserDisplay = False
+                    self.beerChooser = True
+                if self.mouse[0] > (SWIDTH-(50*RELX)) and self.mouse[1] < (50*RELY):
+                    self.running = False
+
+    def openValve(self):
+        GPIO.output(5, True)
+        self.dispensing = True
+
+    def shutValve(self):
+        GPIO.output(5, False)
+        self.dispensing = False
+
+    def dispensorDraw(self):
+        self.pintsLeft = (self.kegVolume()+499)/500
+        try:
+            self.drawToScreen(BACKGROUNDS[self.bg_image], SWIDTH/2, SHEIGHT/2)
+        except IndexError:
+            self.drawToScreen(DEFAULT_BACKGROUND, SWIDTH/2, SHEIGHT/2)
+        if self.dispensing:
+            self.drawToScreen(BUTTON_ON, SWIDTH-(100*RELX), SHEIGHT-(100*RELY))
+        if not self.dispensing:
+            self.drawToScreen(BUTTON, SWIDTH-(100*RELX), SHEIGHT-(100*RELY))
+        self.drawToScreen(PINTS_ICON, (SWIDTH*0.1), (SHEIGHT*0.9))
+        if int(int(self.kegVolume())/500) <= 9:
+            self.drawToScreen(NEON_NUMBER[int(str(self.pintsLeft))], (SWIDTH*0.28), (SHEIGHT*0.9))
+        if int(int(self.kegVolume())/500) > 9:
+            self.drawToScreen(NEON_NUMBER[int(str(self.pintsLeft)[0:1])], ((SWIDTH*0.28)-(30*RELX)), (SHEIGHT*0.9))
+            self.drawToScreen(NEON_NUMBER[int(str(self.pintsLeft)[1:2])], ((SWIDTH*0.28)+(30*RELX)), (SHEIGHT*0.9))
+        self.drawToScreen(QUIT, (25*RELX), (25*RELY))
+        pg.display.flip()
+
+    def beerChooserEvents(self):
+        self.mouse = pg.mouse.get_pos()
+        self.click = pg.mouse.get_pressed()
+        self.keys = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+            if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                self.running = False
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = SWIDTH/2, SHEIGHT/2
+                if self.mouse[0] < (50*RELX) and self.mouse[1] < (50*RELY):
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x-(150*RELX)) < self.mouse[0] < (x+(150*RELX)) and self.mouse[1] < (150*RELY):
+                    self.bg_image = 0  # Default background
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x-(300*RELX)) < self.mouse[0] < (x-(150*RELX)) and (150*RELY) < self.mouse[1] < (300*RELY):
+                    self.bg_image = 1
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x-(150*RELX)) < self.mouse[0] < x and (150*RELY) < self.mouse[1] < (300*RELY):
+                    self.bg_image = 2
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if x < self.mouse[0] < (x+(150*RELX)) and (150*RELY) < self.mouse[1] < (300*RELY):
+                    self.bg_image = 3
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x+(150*RELX)) < self.mouse[0] < (x+(300*RELX)) and (150*RELY) < self.mouse[1] < (300*RELY):
+                    self.bg_image = 4
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x-(300*RELX)) < self.mouse[0] < (x-(150*RELX)) and (300*RELY) < self.mouse[1] < (450*RELY):
+                    self.bg_image = 5
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x-(150*RELX)) < self.mouse[0] < x and (300*RELY) < self.mouse[1] < (450*RELY):
+                    self.bg_image = 6
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if x < self.mouse[0] < (x+(150*RELX)) and (300*RELY) < self.mouse[1] < (450*RELY):
+                    self.bg_image = 7
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x+(150*RELX)) < self.mouse[0] < (x+(300*RELX)) and (300*RELY) < self.mouse[1] < (450*RELY):
+                    self.bg_image = 8
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x-(300*RELX)) < self.mouse[0] < (x-(150*RELX)) and (450*RELY) < self.mouse[1] < (600*RELY):
+                    self.bg_image = 9
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x-(150*RELX)) < self.mouse[0] < x and (450*RELY) < self.mouse[1] < (600*RELY):
+                    self.bg_image = 10
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if x < self.mouse[0] < (x+(150*RELX)) and (450*RELY) < self.mouse[1] < (600*RELY):
+                    self.bg_image = 11
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+                if (x+(150*RELX)) < self.mouse[0] < (x+(300*RELX)) and (450*RELY) < self.mouse[1] < (600*RELY):
+                    self.bg_image = 12
+                    self.dispenserDisplay = True
+                    self.beerChooser = False
+
+    def beerChooserDraw(self):
+        self.drawToScreen(BRICKS, SWIDTH/2, SHEIGHT/2)
+        self.drawToScreen(DEFAULT_THUMBNAIL, SWIDTH/2, (75*RELY))
+        thumbImage = 1
+        ratioX = 0.2
+        ratioY = 0.25
+        for iy in range(3):
+            y = (SHEIGHT*ratioY)
+            for ix in range(4):
+                x = (SWIDTH*ratioX)
+                try:
+                    self.drawToScreen(THUMB[thumbImage], x, y)
+                except IndexError:
+                    self.drawToScreen(COMING_SOON, x, y)
+                ratioX += 0.2
+                thumbImage += 1
+            ratioY += 0.25
+            ratioX = 0.2
+        self.drawToScreen(QUIT, (25*RELX), (25*RELY))
+        pg.display.flip()
+
+    def infoDisplay():
+        lcd.lcd_string('{} ml'.format(self.kegVolume()), 1)
+        lcd.lcd_string('{} *Celcius'.format(self.kegTemp()), 2)
+
+    def kegVolume(self):
+        dryKegVolume = 4000  # Dry weight of keg system
+        wetKegVolume = hx.get_grams() - dryKegVolume
+        return wetKegVolume
+
+    def kegTemp(self):
+        kegTemperature = self.tempSensor.get_temperature()
+        return kegTemperature
+
+    def run(self):
+        self.clock.tick(FPS)
+        if self.dispenserDisplay:
+            self.dispensorEvents()
+            self.dispensorDraw()
+        if self.beerChooser:
+            self.beerChooserEvents()
+            self.beerChooserDraw()
+        self.infoDisplay()
+
+if __name__ == '__main__':
+    b = BeerDispenser()
+    while b.running:
+        try:
+            b.run()
+        except KeyboardInterrupt:
+            b.running = False
+    hx.reset()
+    lcd.lcd_clear()
+    GPIO.cleanup()
+    pg.quit()
--- settings.py
+++ settings.py
@@ -21,23 +21,47 @@ CYAN = (0, 255, 216)
 MAGENTA = (233, 0, 255)
 PURPLE = (179, 0, 255)
 
-# Image files - run through scale transform (optimized for 1024x600)
-dispensor_background = pg.image.load('img/background_screen_info.png')
-DISPENSOR_BACKGROUND = pg.transform.scale(dispensor_background,
-                                          (int(1024*RELX), int(600*RELY)))
-intro_background = pg.image.load('img/splash_screen.png')
-INTRO_BACKGROUND = pg.transform.scale(intro_background,
-                                      (int(1024*RELX), int(600*RELY)))
+# Image files - run through scale transform to be compatible between various
+# screen sizes (optimized for 1024x600)
+
+# Background images
+bricks = pg.image.load('img/brick_wall.png')
+BRICKS = pg.transform.scale(bricks, (int(1024*RELX), int(600*RELY)))
+BACKGROUNDS = []
+default_background = pg.image.load('img/default_background.png')
+DEFAULT_BACKGROUND = pg.transform.scale(default_background, (int(1024*RELX), int(600*RELY)))
+BACKGROUNDS.append(DEFAULT_BACKGROUND)
+tt_background = pg.image.load('img/tropical_thunder_bg.png')
+TT_BACKGROUND = pg.transform.scale(tt_background, (int(1024*RELX), int(600*RELY)))
+BACKGROUNDS.append(TT_BACKGROUND)
+
+# Beer thumbnails
+THUMB = []
+default_thumbnail = pg.image.load('img/default_icon.png')
+DEFAULT_THUMBNAIL = pg.transform.scale(default_thumbnail, (int(300*RELX), int(150*RELY)))
+THUMB.append(DEFAULT_THUMBNAIL)
+beer1 = pg.image.load('img/tropical_thunder.png')
+BEER1 = pg.transform.scale(beer1, (int(150*RELX), int(150*RELY)))
+THUMB.append(BEER1)
+coming_soon = pg.image.load('img/coming_soon_thumb.png')
+COMING_SOON = pg.transform.scale(coming_soon, (int(150*RELX), int(150*RELY)))
+
+
+# Buttons & icons
 button = pg.image.load('img/beer_button.png')
 BUTTON = pg.transform.scale(button, (int(200*RELX), int(200*RELY)))
 button_on = pg.image.load('img/beer_button1.png')
 BUTTON_ON = pg.transform.scale(button_on, (int(200*RELX), int(200*RELY)))
 quit = pg.image.load('img/exit.png')
 QUIT = pg.transform.scale(quit, (int(50*RELX), int(50*RELY)))
-delete = pg.image.load('img/delete.png')
-DELETE = pg.transform.scale(delete, (int(100*RELX), int(100*RELY)))
-done = pg.image.load('img/done.png')
-DONE = pg.transform.scale(done, (int(100*RELX), int(100*RELY)))
+ml_icon = pg.image.load('img/ml_icon.png')
+ML_ICON = pg.transform.scale(ml_icon, (int(50*RELX), int(50*RELY)))
+temp_icon = pg.image.load('img/temp_icon.png')
+TEMP_ICON = pg.transform.scale(temp_icon, (int(50*RELX), int(50*RELY)))
+pints_icon = pg.image.load('img/pints.png')
+PINTS_ICON = pg.transform.scale(pints_icon, (int(200*RELX), int(100*RELY)))
+
+# Image files for numbers
 NEON_NUMBER = []
 enum = 0
 for img in range(10):
@@ -56,6 +80,6 @@ NEON_NUMBER_SCALED_MINI = []
 enum_scaled_mini = 0
 for img in range(10):
     unscaled_img = pg.image.load('img/num{}.png'.format(enum_scaled_mini))
-    img = pg.transform.scale(unscaled_img, (int(30*RELX), int(30*RELY)))
+    img = pg.transform.scale(unscaled_img, (int(25*RELX), int(25*RELY)))
     NEON_NUMBER_SCALED_MINI.append(img)
     enum_scaled_mini += 1
