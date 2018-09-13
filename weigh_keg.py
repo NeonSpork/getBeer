7d07700d004807e1c37a6be540fc1fc40f9d5a38
NeonSpork <leiknes@gmail.com>
2018-09-13 21:51:31 +0200

    updated weigh_keg.py with new HX711 class

--- weigh_keg.py
+++ weigh_keg.py
@@ -10,8 +10,11 @@ from hx711 import HX711
 
 
 print("Scale initializing...")
-hx = HX711(2, 3)
+hx = HX711(dout_pin=2, pd_sck_pin=3, gain_channel_A=64, select_channel='A')
+hx.set_offset(100350, channel='A', gain_A=64)
+hx.set_scale_ratio(channel='A', gain_A=64, scale_ratio=36.2109)
 print("Ready.\n")
-dryWeight = hx.get_grams()
-print("Dry weight of keg is {} grams".format(dryWeight))
+dryWeight = hx.get_weight_mean(times=10)
+print("Average dry weight of keg is {} grams".format(dryWeight))
+hx.reset()
 GPIO.cleanup()
