e0c8a41a8a3def8474328b28cd744b18ee2420e1
NeonSpork <leiknes@gmail.com>
2018-11-08 23:50:03 +0100

    Updating .gitignore

--- getBeer_cpp/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programs/Coding_sandbox/getBeer/getBeer/getBeer_cpp/tests
-# Build directory: D:/Programs/Coding_sandbox/getBeer/getBeer/getBeer_cpp/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
