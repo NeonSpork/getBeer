363dc18dee2c1c70cc7477a7fe1ae29ba19f9ef9
NeonSpork <leiknes@gmail.com>
2018-11-10 21:29:35 +0100

    Updating autolaunch.sh tp new correct file layout

--- getBeer_python/autolaunch.sh
+++ getBeer_python/autolaunch.sh
@@ -3,13 +3,13 @@
 ### GUI / desktop version
 # $ sudo nano /home/pi/.config/lxsession/LXDE-pi/autostart
 # add the following line at the bottom:
-# @sh /home/pi/getBeer/autolaunch.sh
+# @sh /home/pi/getBeer/getBeer_python/autolaunch.sh
 ### CLI version
 # $ crontab -e
 # add the following line at the bottom:
-# @reboot /home/pi/getBeer/autolaunch.sh > /home/pi/getBeer/beer.log 2>&1
+# @reboot /home/pi/getBeer/getBeer_python/autolaunch.sh > /home/pi/getBeer/beer.log 2>&1
 
-cd /home/pi/getBeer
+cd /home/pi/getBeer/getBeer_python
 /usr/bin/python3 getBeer.py
 # change the file name to whatever is appropriate
 # for example getBeer_touch.py or getBeer_headless.py
