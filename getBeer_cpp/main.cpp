9239d40e56b3990f9eed4e725c830f99d0ab43bb
NeonSpork <leiknes@gmail.com>
2018-11-10 21:42:45 +0100

    Updating secret branch to be concurrent with master

---
+++ getBeer_cpp/main.cpp
@@ -0,0 +1,14 @@
+// getBeer
+//
+// A framework for a custom beer dispenser and kegerator
+// running on a Raspberry Pi, a touch screen, a bunch of
+// electronics and an old fridge.
+
+#include "./include/Dispenser.hpp"
+
+int main()
+{
+    Dispenser getBeer;
+    getBeer.run();
+    return 0;
+}
---
+++ getBeer_cpp/src/Dispenser.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "../include/Dispenser.hpp"
+
+Dispenser::Dispenser(){};
+void Dispenser::run()
+{
+  sf::Clock clock;
+}
