4b7c0d4dbc382805e7cc4e62031fecaf2e539e13
NeonSpork <33250853+NeonSpork@users.noreply.github.com>
2018-11-10 19:33:56 +0100

    Update autolaunch.sh

--- getBeer_python/autolaunch.sh
+++ getBeer_python/autolaunch.sh
@@ -9,7 +9,7 @@
 # add the following line at the bottom:
 # @reboot /home/pi/getBeer/autolaunch.sh > /home/pi/getBeer/beer.log 2>&1
 
-cd /home/pi/getBeer
+cd /home/pi/getBeer/getBeer_python
 /usr/bin/python3 getBeer.py
 # change the file name to whatever is appropriate
 # for example getBeer_touch.py or getBeer_headless.py
