dcb4290ada8103b7f43557d323e941a7bf624085
NeonSpork <leiknes@gmail.com>
2018-11-11 14:58:38 +0100

    Adding more to c++ framework

--- getBeer_cpp/include/Dispenser.hpp
+++ getBeer_cpp/include/Dispenser.hpp
@@ -11,11 +11,17 @@ public:
     void run();
 private:
     void processEvents();
-    void update();
+    void handleKeyboardInput(sf::Keyboard::Key key, bool isPressed);
+    void handleMouseInput(sf::Mouse::Button button, bool isPressed);
+    void update(sf::Time deltaTime);
     void render();
 
 private:
     sf::RenderWindow mWindow;
+    sf::Texture mTexture;
+    sf::Sprite mBackground;
+    bool mUserKeyboardExit;
+    const sf::Time TimePerFrame = sf::seconds(1.f/60.f);
 };
 
 #endif
