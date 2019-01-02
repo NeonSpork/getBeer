f9218eb7658172ecc33a51f970cbd5519d38b65d
NeonSpork <leiknes@gmail.com>
2019-01-02 09:04:42 +0100

    Deleting superfluous folders

--- getBeer_sfml_complex/readme.md
+++
@@ -1,7 +0,0 @@
-# C++ version
-
-Updated version that runs on C++
-
-Will hopefully be faster at runtime. Utilizes SFML library.
-
-# WORK IN PROGRESS
--- getBeer_sfml_complex/src/Valve.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/Valve.hpp"
-#include <cmath>
-
-Valve::Valve(unsigned int pin)
-: mValvePin{GPIO::DigitalOut(pin)}
-{
-  // Valve GPIO pin starts closed
-  mValvePin.off();
-}
