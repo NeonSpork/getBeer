c2fc8d38b13181ca79ff2720824535a351867632
NeonSpork <leiknes@gmail.com>
2019-01-02 10:29:58 +0100

    Adjusted comment explaining button grid display when in beer menu state

--- getBeer_sfml/src/App.cpp
+++ getBeer_sfml/src/App.cpp
@@ -267,10 +267,15 @@ void App::handleInput(sf::Mouse::Button button, bool isPressed)
     if (mState == State::ID::BeerMenu)
     {
       /* BUTTON GRID
-                00
-            1  2  3  4
-            5  6  7  8
-            9 10 11 12
+        +--+--+--+--+
+        |  |00000|  |
+        +--+--+--+--+
+        |01|02|03|04|
+        +--+--+--+--+
+        |05|06|07|08|
+        +--+--+--+--+
+        |09|10|11|12|
+        +--+--+--+--+
       */
       // To be updated with graphics as more beers get added to repetoire
       int xx = wWidth/2;
