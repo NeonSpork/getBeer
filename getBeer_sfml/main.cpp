6302f50258951adc3d8f7f0071647ac2a684212f
NeonSpork <33250853+NeonSpork@users.noreply.github.com>
2019-01-04 15:21:56 +0100

    Update main.cpp

--- getBeer_sfml/main.cpp
+++ getBeer_sfml/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[])
 {
   // Must be called at beginning of program to function correctly
-  wiringPiSetupGpio();
+  // wiringPiSetupGpio();
   pinMode(5, OUTPUT); // Beer valve
   pinMode(6, OUTPUT); // Secret valve
 
