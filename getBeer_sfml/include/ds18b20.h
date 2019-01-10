209f69c2c441b532490a5d11195fa4b810c37511
NeonSpork <leiknes@gmail.com>
2019-01-10 22:13:44 +0100

    Forgot to add the path to the individual device, resulting in no temp reads

--- getBeer_sfml/include/ds18b20.h
+++ getBeer_sfml/include/ds18b20.h
@@ -27,7 +27,7 @@ private:
   char buf[256];     // Data from device
   char tmpData[6];   // Temp C * 1000 reported by device 
   ssize_t numRead;
-  static constexpr char path[] = "/sys/bus/w1/devices";
+  char path[] = "/sys/bus/w1/devices";
 };
 
 #endif /* DS18B20_H_ */
