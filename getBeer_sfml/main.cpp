9f37bd0de0dbfaf4bd54b30119c70fa409cbd2ba
NeonSpork <leiknes@gmail.com>
2019-06-22 22:15:34 +0200

    update main.cpp

--- getBeer_sfml/main.cpp
+++ getBeer_sfml/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[])
 {
   // Must be called at beginning of program to function correctly
   wiringPiSetupGpio();
+  std::cout << "wiringPi complete";
   
   App getBeer;
   getBeer.run();
