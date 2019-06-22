900423f61c44d2b38b363b5b2c7cd14712645ede
NeonSpork <leiknes@gmail.com>
2019-06-22 23:38:20 +0200

    typo fix

--- getBeer_sfml/HX711_calibration/main.cpp
+++ getBeer_sfml/HX711_calibration/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[])
   long averageVal = hx.averageValue();
   hx.setOffset(averageVal);
   char cont = 'a';
-  std::cout << "!!!!! Remove all items from scale !!!!!\nress any key to continue.\n";
+  std::cout << "!!!!! Remove all items from scale !!!!!\nPress any key to continue.\n";
   std::cin >> cont;
   long offset = hx.getOffset();
   std::cout << "Place item with known weight on scale.\nPress any key to continue.\n";
