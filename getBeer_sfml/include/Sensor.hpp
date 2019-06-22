686622e340c8b3dc972d5b532f9537bf2d24bd1e
NeonSpork <leiknes@gmail.com>
2019-06-22 21:58:56 +0200

    Removing temp sensor, it doesn't work on tinkerboard in current state.

--- getBeer_sfml/include/Sensor.hpp
+++ getBeer_sfml/include/Sensor.hpp
@@ -2,7 +2,7 @@
 #define SENSOR_HPP
 
 #include "hx711.h"
-#include "ds18b20.h"
+// #include "ds18b20.h"
 
 class App;
 
@@ -15,7 +15,7 @@ public:
   class SensorFail{};
 private:
   HX711 hx;
-  Ds18b20 temp;
+  // Ds18b20 temp;
 };
 
 #endif
