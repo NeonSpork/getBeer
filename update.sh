182de61ddbc59d42c0720190113d257b3446abac
NeonSpork <leiknes@gmail.com>
2019-06-24 00:27:19 +0200

    Adding clarifying comment about how the autolaunch doesn't want to cooperate on Asus Tinker Board.

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# Asus Tinker Board:
+# sudo nano /etc/xdg/lxsession/LXDE/autostart
+# @sh ~/getBeer/update.sh
+# However, it is SUPER hard to get this working properly apparently
+# Recommend copying this shell script to desktop and double clicking it
+# to start getBeer...
 
 cd ~/getBeer/getBeer_sfml
 /usr/bin/git pull
